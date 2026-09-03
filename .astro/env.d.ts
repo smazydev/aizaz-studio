fa25e14ddea3b0b81913a9d6c232b59e80557f07
Ali Zafar <alizafar1437@gmail.com>
2026-09-03 19:26:41 +0500

    polish: refine case study pages and image handling

--- .astro/env.d.ts
+++ .astro/env.d.ts
@@ -1,7 +1,12 @@
-declare module 'astro:env/server' {
+declare module 'astro:env/client' {
+	export const PUBLIC_TURNSTILE_SITE_KEY: string | undefined;	
+	export const PUBLIC_GOOGLE_ADS_SEND_TO: string | undefined;	
+}declare module 'astro:env/server' {
 	export const SANITY_PROJECT_ID: string;	
 	export const SANITY_DATASET: string;	
 	export const REVALIDATE_SECRET: string | undefined;	
+	export const TURNSTILE_SECRET_KEY: string | undefined;	
+	export const TURNSTILE_PREVIEW_HOST: string | undefined;	
 	export const CLOUDFLARE_ZONE_ID: string | undefined;	
 	export const CLOUDFLARE_API_TOKEN: string | undefined;	
 }
